8cb2af39547937c500c19f8a3405bc1f3de2915f
aricsa <topww00@naver.com>
2023-11-29 17:26:32 +0900

    set mandatory files

--- src/PuzzlePiece.java
+++
@@ -1,9 +0,0 @@
-public class PuzzlePiece {
-    private int face_value;
-
-    public PuzzlePiece(int value)
-    {
-        face_value = value;
-    }
-    public int faceValue() { return face_value; }
-}
